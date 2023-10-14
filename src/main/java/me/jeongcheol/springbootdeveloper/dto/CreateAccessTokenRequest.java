97095aa4fba8d873730a563633473fc2272ddf5c
tree4553 <tree4553@naver.com>
2023-10-14 17:03:41 +0900

    RefreshToken 기능 구현 및 테스트 구현

---
+++ src/main/java/me/jeongcheol/springbootdeveloper/dto/CreateAccessTokenRequest.java
@@ -0,0 +1,10 @@
+package me.jeongcheol.springbootdeveloper.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CreateAccessTokenRequest {
+    private String refreshToken;
+}
